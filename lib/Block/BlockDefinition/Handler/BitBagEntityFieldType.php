f45fbdcae5dcb34c4b2d29ea92226f4286e78235
Edi Modrić <edi.modric@gmail.com>
2025-12-15 22:37:29 +0100

    CS fix

--- lib/Block/BlockDefinition/Handler/BitBagEntityFieldType.php
+++ lib/Block/BlockDefinition/Handler/BitBagEntityFieldType.php
@@ -7,16 +7,10 @@ namespace Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler;
 enum BitBagEntityFieldType: string
 {
     case String = 'string';
-
     case Number = 'number';
-
     case Media = 'media';
-
     case DateTime = 'datetime';
-
     case Boolean = 'boolean';
-
     case Other = 'other';
-
     case Content = 'content';
 }
